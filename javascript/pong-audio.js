be883f6b9eaa779bd632e0fb7154ea3081a4e100
Vic <154265232+chromatic-creations@users.noreply.github.com>
2024-01-12 10:28:39 -0600

    Attempt with ambient sound at full volume

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -56,7 +56,8 @@ scoreSound.player.volume.value = -10;
 export var ambientSound = new soundFile("ambience.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -7; //turn down volume
+// ambientSound.player.volume.value = -7;
+//turn down volume
 
 export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
