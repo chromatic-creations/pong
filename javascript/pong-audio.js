755b036dbdae6ae96ae10b45c3073c2f8eb9fed7
Vic <154265232+chromatic-creations@users.noreply.github.com>
2024-01-12 09:53:57 -0600

    Increase ambient audio volume

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -56,7 +56,7 @@ scoreSound.player.volume.value = -10;
 export var ambientSound = new soundFile("ambience.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -15; //turn down volume
+ambientSound.player.volume.value = -10; //turn down volume
 
 export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
